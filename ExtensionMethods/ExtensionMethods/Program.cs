afa9c091b9c902dabdf087d7d00ae8969a057d44
marcBiason <54872072+marcioBiason@users.noreply.github.com>
2019-10-08 14:19:49 -0300

    Add files via upload

---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2019, 10, 8, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
